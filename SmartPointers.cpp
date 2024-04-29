d3697cfbace6076acb382567b9981d01d570b2c0
Pagalavan <125946240+PagalavanPagal66@users.noreply.github.com>
2024-04-29 17:52:58 +0530

    Update SmartPointers

--- SmartPointers.cpp
+++ SmartPointers.cpp
@@ -9,6 +9,9 @@ public:
 	int value() {
 		return *ptr;
 	}
+	void changeval() {
+		(*ptr)++;
+	}
 	~SmartPtr() {cout << "DELETED" << endl; delete (ptr); }
 	int& operator*() { return *ptr; }
 };
@@ -17,9 +20,15 @@ int main() {
 	int value = 10;
 	int* ptr = &value;
 	unique_ptr<SmartPtr> P1(new SmartPtr(ptr));
-	cout << P1->value() << endl;
-	unique_ptr<SmartPtr>P2;
-	P2 = move(P1);
+	//cout << P1->value() << endl;
+	unique_ptr<SmartPtr> P2(new SmartPtr(ptr));
+	//P2 = move(P1);
+	// cout << P2->value() << endl;
+	// cout << P1->value() << endl;
+	// shared_ptr<SmartPtr> P3(new SmartPtr(ptr));
+	// weak_ptr<SmartPtr> P4 (P3);
+	// cout << P3->value() << endl;
+	P1->changeval();
 	cout << P2->value() << endl;
 	cout << P1->value() << endl;
 }
